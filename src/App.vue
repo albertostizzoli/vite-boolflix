68dcff9f26a8df30624e2b503fdc80a9d2861f13
albertostizzoli <albertostizzoli@yahoo.it>
2023-11-15 13:28:59 +0100

    Complete Milestone 0 and added file README

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,57 @@
 <template>
-  <div>
+  <main>
+    <section id="movie" class="container">
+      <h2>Movies</h2>
+      <div class="row">
+        <div class="col-12 col-md-4 col-lg-3" v-for="(movie, index) in store.movieList" :key="movie.id">
+          {{ movie.title }}
+        </div>
+      </div>
+    </section>
+    <section id="series" class="container">
+      <h2>Tv</h2>
+      <div class="row">
+        <div class="col-12 col-md-4 col-lg-3" v-for="(serie, index) in store.seriesList" :key="serie.id">
+          {{ serie.name }}
+          <img src="'https://'" alt="">
+        </div>
+      </div>
+    </section>
 
-  </div>
+  </main>
 </template>
 
 <script>
 import axios from 'axios'
 import { store } from './store.js'
-  export default {
-    name: 'App',
-    data(){
-      return{
-        store
-      }
+export default {
+  name: 'App',
+  data() {
+    return {
+      store
     }
-    
+  },
+  methods: {
+    getMoviesAndSeries() {
+      const movieurl = store.apiUrl + this.store.endPoint.movies;
+      axios.get(movieurl, { params: this.store.params }).then((res) => {
+        console.log(res.data.results);
+        this.store.movieList = res.data.results;
+      });
+      const tvurl = store.apiUrl + this.store.endPoint.series;
+      axios.get(tvurl, { params: this.store.params }).then((res) => {
+        console.log(res.data.results);
+        this.store.seriesList = res.data.results;
+      });
+    }
+  },
+  created() {
+    this.getMoviesAndSeries();
+
   }
+}
+
 </script>
 
 <style lang="scss" scoped>
-
-</style>
 </style>
--- src/store.js
+++ src/store.js
@@ -1,4 +1,17 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3',
+    endPoint: {
+        movies:"search/movie",
+        series: "search/tv"
+    },
+
+    api: '57c1fd25fdb62d9a100ae60009b3bf0b',
+    query: 'a',
+    error:"",
+    loading: true,
+    movieList: [],
+    seriesList: []
+
 })
