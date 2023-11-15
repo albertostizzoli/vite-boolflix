3410b477d8524078f4ea8c169e5fa41f7af7cc20
albertostizzoli <albertostizzoli@yahoo.it>
2023-11-15 12:21:05 +0100

    Create Repo and Scaffolding

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { store } from './store.js'
+  export default {
+    name: 'App',
+    data(){
+      return{
+        store
+      }
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+</style>
